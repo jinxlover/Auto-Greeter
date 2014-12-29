9065bff1cca1dabd410d685ba82279e197aa55ad
jinxlover <dude2043099@gmail.com>
2014-12-29 11:27:45 -0600

    Update baby

--- Auto-Greeter/Program.cs
+++ Auto-Greeter/Program.cs
@@ -22,10 +22,7 @@ namespace Auto_Greeter
             if (Game.Time < 10)
             {
                 //First Assembly ever made, already better than BLM Kappa
-                //If anyone asks this is a Keyboard Macro (Lenny Face)
-                //Added Delay so no need to say that you have a Keyboard Macro anymore (Kappa)
-                //Removed Delay cause it was delaying IF GAME.TIME and still posting messages after 10 seconds.
-                // I'm stumped, Spams the message whenever you hit f5, can't figure out why. Game.Time doesn't seem to be working > 10000
+                // Wee I did it!
                  Utility.DelayAction.Add(2000, () => Game.Say("Good luck Have fun team, Let's win this we dem boyz"));
                  Utility.DelayAction.Add(5000, () => Game.Say("Also, don't feed and don't rage, smoke weed everyday"));
 
