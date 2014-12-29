90abb487ca6b3e4a69eb2a52f745036ae2cf2e21
jinxlover <dude2043099@gmail.com>
2014-12-28 18:17:18 -0600

    V1

--- Auto-Greeter/Program.cs
+++ Auto-Greeter/Program.cs
@@ -19,7 +19,7 @@ namespace Auto_Greeter
 
         static void Game_OnGameLoad(EventArgs args)
         {
-            if (Game.Time > 10)
+            if (Game.Time < 10)
             {
                 //First Assembly ever made, already better than BLM Kappa
                 //If anyone asks this is a Keyboard Macro (Lenny Face)
